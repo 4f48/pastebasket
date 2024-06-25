8c03a4aa946fae91cda614ffc934bda3476e25b9
4f48 <0x4f48@proton.me>
2024-06-25 18:38:50 +0200

    Rename environment variable to match with Vercel

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.POSTGRES_URL) throw new Error('POSTGRES_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'postgresql',
 
 	dbCredentials: {
-		url: process.env.DATABASE_URL
+		url: process.env.POSTGRES_URL
 	},
 
 	verbose: true,
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.POSTGRES_URL) throw new Error('POSTGRES_URL is not set');
 
-const client = neon(env.DATABASE_URL);
+const client = neon(env.POSTGRES_URL);
 export const db = drizzle(client);
