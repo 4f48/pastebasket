0090dbde692cfb1863ae4c48b9dac2c0c7f643cf
4f48 <0x4f48@proton.me>
2024-06-25 19:02:52 +0200

    Add components for form from shadcn_svelte

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -3,7 +3,7 @@ import { boolean, pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/
 export const pastes = pgTable('pastes', {
 	id: serial('id').unique().primaryKey(),
 	title: varchar('title', { length: 50 }).notNull(),
-	content: text("content").notNull(),
-	timestamp: timestamp("timestamp").defaultNow().notNull(),
-	listed: boolean("listed").default(true).notNull(),
+	content: text('content').notNull(),
+	timestamp: timestamp('timestamp').defaultNow().notNull(),
+	listed: boolean('listed').default(true).notNull()
 });
