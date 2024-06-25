4c5f1d37073ce0aa9ff19b240e230b3433ec4a28
4f48 <0x4f48@proton.me>
2024-06-25 18:04:53 +0200

    Setup development environment

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+            corepack_20
+          ];
+        };
+      }
+    );
+}
