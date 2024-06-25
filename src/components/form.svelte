cf91ddb1e87b47078f39a697c406ee3f32fcb08e
4f48 <0x4f48@proton.me>
2024-06-25 19:13:36 +0200

    Setup form

---
+++ src/components/form.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import * as Form from "$lib/components/ui/form";
+    import { Input } from "@/components/ui/input";
+    import { Textarea } from "@/components/ui/textarea";
+    import { formSchema, type FormSchema } from "@/forms";
+    import {
+      type SuperValidated,
+      type Infer,
+      superForm,
+    } from "sveltekit-superforms";
+    import { zodClient } from "sveltekit-superforms/adapters";
+   
+    export let data: SuperValidated<Infer<FormSchema>>;
+   
+    const form = superForm(data, {
+      validators: zodClient(formSchema),
+    });
+   
+    const { form: formData, enhance } = form;
+  </script>
+   
+  <form method="POST" use:enhance>
+    <Form.Field {form} name="title">
+      <Form.Control let:attrs>
+        <Form.Label>Title</Form.Label>
+        <Input {...attrs} bind:value={$formData.title} />
+      </Form.Control>
+      <Form.FieldErrors />
+    </Form.Field>
+    <Form.Field {form} name="content">
+        <Form.Control let:attrs>
+          <Form.Label>Content</Form.Label>
+          <Textarea {...attrs} bind:value={$formData.content} />
+        </Form.Control>
+        <Form.FieldErrors />
+      </Form.Field>
+    <Form.Button>Submit</Form.Button>
+  </form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import type { PageData } from "./$types.js";
+    import SettingsForm from "../components/form.svelte";
+    export let data: PageData;
+  </script>
+   
+  <SettingsForm data={data.form} />
