171f931b388827a4f1b1e66be2d0211083fada33
4f48 <0x4f48@proton.me>
2024-06-27 13:53:58 +0200

    Basic header

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,4 +1,9 @@
-<header class="flex w-full gap-4 bg-muted">
-	<a href="/">Pastebasket</a>
-	<a href="/search">Search</a>
+<script lang="ts">
+	import { Button } from "@/components/ui/button";
+	import { Search, ShoppingBasket } from "lucide-svelte";
+</script>
+
+<header class="flex w-full gap-4 bg-muted items-center">
+	<a href="/"><Button size="lg" variant="link" class="font-bold text-xl flex gap-2 items-center"><ShoppingBasket /> Pastebasket</Button></a>
+	<div class="flex flex-1 justify-end"><a href="/search"><Button variant="link" size="icon"><Search size="18px" /></Button></a></div>
 </header>
