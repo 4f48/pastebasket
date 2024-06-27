291fbfeb34bd3b7e883d786e65a9cf2972d03db0
4f48 <0x4f48@proton.me>
2024-06-27 12:29:33 +0200

    Add API route to query baskets by ID

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,8 +1,9 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import { env } from '$env/dynamic/private';
+import * as schema from './schema';
 
 if (!env.POSTGRES_URL) throw new Error('POSTGRES_URL is not set');
 
 const client = neon(env.POSTGRES_URL);
-export const db = drizzle(client);
+export const db = drizzle(client, { schema });
---
+++ src/routes/api/query/[slug]/+server.ts
@@ -0,0 +1,13 @@
+import type { RequestHandler } from '../$types';
+import { db } from '@/server/db/index';
+import { baskets } from '@/server/db/schema';
+import { eq } from 'drizzle-orm';
+import { json } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async ({ params }) => {
+    const response = await db.query.baskets.findFirst({
+		where: eq(baskets.id, params.slug)
+	});
+
+	return json(response);
+};
