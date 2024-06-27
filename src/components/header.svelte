17255178cf088bb692616edab9a255b6cea91fc1
4f48 <0x4f48@proton.me>
2024-06-27 12:34:14 +0200

    Add basic header for navigation

---
+++ src/components/header.svelte
@@ -0,0 +1,4 @@
+<header class="flex gap-4 w-full bg-muted">
+    <a href="/">Pastebasket</a>
+    <a href="/search">Search</a>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script>
 	import '../app.css';
 	import { Toaster } from '$lib/components/ui/sonner';
+
+	import Header from '../components/header.svelte';
 </script>
 
 <Toaster />
+
+<Header />
 <slot />
 
 <style></style>
